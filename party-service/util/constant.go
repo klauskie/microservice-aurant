c040147b1ab156225b64ca8602ef93ca975c95cd
Klaus Kientzle <klauskt@hotmail.com>
2021-04-28 19:53:30 -0600

    Setup for Docker containers completed for develop

--- party-service/util/constant.go
+++ party-service/util/constant.go
@@ -3,6 +3,6 @@ package util
 const (
 	URL_ORDER_API            = "http://localhost:8082"
 	URL_ORDER_API_SEND_BATCH = URL_ORDER_API + "/api-order/batch"
-	URL_SESSION_API          = "http://localhost:8083/session-api"
-	URL_CATALOG_API          = "http://localhost:8080/api"
+	URL_SESSION_API          = "http://api-session:8083/session-api"
+	URL_CATALOG_API          = "http://api-catalog:8080/api"
 )
