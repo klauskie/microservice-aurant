3c63190e5b3f6487452c3b76380938478f89c0d9
Klaus Kientzle <klauskt@hotmail.com>
2021-05-08 17:18:07 -0600

    prepare for demo production

--- aurant-front-demo/src/util/Constants.js
+++ aurant-front-demo/src/util/Constants.js
@@ -1,6 +1,6 @@
-export const CATALOG_API_URL = 'http://localhost:8080'
-export const PARTY_API_URL = 'http://localhost:8081'
-export const SESSION_API_URL = 'http://localhost:8083'
+export const CATALOG_API_URL = 'http://api-catalog:8080'
+export const PARTY_API_URL = 'http://api-party:8081'
+export const SESSION_API_URL = 'http://api-session:8083'
 
 export const KEY_ACCESS_TOKEN = 'access_token'
 export const KEY_TAG = 'tag'
