bff9f306ec1bfd828b51866386ddb03422917466
Klaus Kientzle <klauskt@hotmail.com>
2021-05-10 00:53:29 +0000

    cloud setup

--- aurant-front-demo/src/util/Constants.js
+++ aurant-front-demo/src/util/Constants.js
@@ -1,6 +1,6 @@
-export const CATALOG_API_URL = 'http://api-catalog:8080'
-export const PARTY_API_URL = 'http://api-party:8081'
-export const SESSION_API_URL = 'http://api-session:8083'
+export const CATALOG_API_URL = 'http://159.65.44.101:8080'
+export const PARTY_API_URL = 'http://159.65.44.101:8081'
+export const SESSION_API_URL = 'http://159.65.44.101:8083'
 
 export const KEY_ACCESS_TOKEN = 'access_token'
 export const KEY_TAG = 'tag'
@@ -14,7 +14,7 @@ export const STATE_WAIT_ROOM = 2
 export const STATE_MENU = 3
 export const STATE_ORDER = 4
 
-export const VENDOR_ID = "74d760b9-83cc-4baa-bdbb-9e07debb58e1"
+export const VENDOR_ID = "a54ace60-4980-4ee2-929c-6bd07b330499"
 
 // Helper Functions
 const getTokenKey = (clientNumber) => {
